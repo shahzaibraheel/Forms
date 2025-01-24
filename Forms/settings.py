5bab591407a4b32d738107f80620e0a6f1b92e87
shahzaib69613 <shahzaibraheel68@gmail.com>
2025-01-24 12:20:32 +0500

    Update settings.py

--- Forms/settings.py
+++ Forms/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import pymongo
-import dns
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
