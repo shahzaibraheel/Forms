9d15b4ef9ce04a16f9e3774bc5f99171345a4b3f
shahzaibraheel <shahzaibraheel69613@gmail.com>
2025-02-11 22:31:19 +0500

    Update settings.py

--- Forms/settings.py
+++ Forms/settings.py
@@ -49,7 +49,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'myapp',
     'django.contrib.admin',
-    'django.contrib.auth',
+ 
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
