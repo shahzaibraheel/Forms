3b0d339cbc812a12818756bca4d9fdfc7ef27fd8
shahzaib69613 <shahzaibraheel68@gmail.com>
2025-02-11 16:40:46 +0500

    Update firebase_config.py

--- myapp/firebase_config.py
+++ myapp/firebase_config.py
@@ -1,16 +1,18 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-
 import os
+import base64
+import json
 
-# Get the base directory of the Django project
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Decode the base64 Firebase credentials from the environment variable
+firebase_creds = json.loads(
+    base64.b64decode(os.getenv("FIREBASE_CREDENTIALS")).decode()
+)
 
-# Construct the full path to Dso.json inside the "myapp" directory
-FIREBASE_CREDENTIALS = os.path.join(BASE_DIR, "myapp", "Dso.json")
-
+# Initialize Firebase only if not already initialized
 if not firebase_admin._apps:
-    cred = credentials.Certificate(FIREBASE_CREDENTIALS)
+    cred = credentials.Certificate(firebase_creds)
     firebase_admin.initialize_app(cred)
 
+# Get Firestore client
 db = firestore.client()
