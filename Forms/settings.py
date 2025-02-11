8d00d7e4d3e6c5cf48f38d1d457d9e2a9934d5a3
shahzaibraheel <shahzaibraheel69613@gmail.com>
2025-02-12 00:44:36 +0500

    Update settings.py

--- Forms/settings.py
+++ Forms/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,6 +157,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+SESSION_ENGINE = "django.contrib.sessions.backends.file"
+SESSION_FILE_PATH = "/tmp/django_sessions"
 
 
 # Static files (CSS, JavaScript, Images)
