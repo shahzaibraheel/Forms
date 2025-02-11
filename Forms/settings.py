de712496f26f07a82004c7a4ede467662060969b
shahzaibraheel <shahzaibraheel69613@gmail.com>
2025-02-11 23:45:02 +0500

    Update settings.py

--- Forms/settings.py
+++ Forms/settings.py
@@ -102,12 +102,15 @@ from urllib.parse import quote_plus
 #         'PORT': '3306',                # Default MySQL port
 #     }
 # }
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),  # Store SQLite in /tmp
     }
 }
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
