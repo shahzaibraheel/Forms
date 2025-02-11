6d1001fe61c862d03858ebeb8b95f61fd41701a4
shahzaibraheel <shahzaibraheel69613@gmail.com>
2025-02-12 00:59:13 +0500

    Update settings.py

--- Forms/settings.py
+++ Forms/settings.py
@@ -50,7 +50,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'myapp',
     'django.contrib.admin',
-    'django.contrib.auth',
+    # 'django.contrib.auth',
     'django.contrib.contenttypes',
     # 'django.contrib.sessions',
     'django.contrib.messages',
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     # 'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
